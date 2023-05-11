b2cff4347718a6f9b70df36fb9090793915406db
Rashid Khalil <38354635+rashidkhalil9182@users.noreply.github.com>
2023-05-11 01:11:38 -0700

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -157,6 +157,7 @@
    */
   const typed = select('.typed')
   if (typed) {
+    // console.log("asd");
     let typed_strings = typed.getAttribute('data-typed-items')
     typed_strings = typed_strings.split(',')
     new Typed('.typed', {
